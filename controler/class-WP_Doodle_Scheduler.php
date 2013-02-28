77f17a28accb857dffd1cf34f9914d3690b94b67
David Naber <kontakt@dnaber.de>
2013-02-28 11:04:41 +0100

    scheduler

--- controler/class-WP_Doodle_Scheduler.php
+++ controler/class-WP_Doodle_Scheduler.php
@@ -8,13 +8,19 @@
 
 class WP_Doodle_Scheduler {
 
-
 	/**
 	 * instance
 	 *
 	 * @var WP_Doodle_Scheduler
 	 */
 	private static $instance = NULL;
+
+	/**
+	 * meta keys represetns the cron_schedules
+	 *
+	 * @var array
+	 */
+	protected $scheduled_keys = array();
 
 	/**
 	 * get the instance
@@ -42,13 +48,37 @@ class WP_Doodle_Scheduler {
 	protected function init( $plugin ) {
 
 		# add post meta to new(!) polls with the default sync-intervall
+		$schedules = wp_get_schedules();
+		foreach ( $schedules as $s ) {
+			self::$scheduled_keys[ $s ] = '_wpdp_' . $s;
+		}
 
 		/**
 		 * meta keys die die intervall-slugs enthalten:
 		 * _wpdp_never => 6 // 0 bis 6 uhr gar nicht syncen
-		 * _wpdp_hourl => 18 // 6 bis 18 uhr stündlich syncen
+		 * _wpdp_hourly => 18 // 6 bis 18 uhr stündlich syncen
 		 * _wpdp_twicedaly => 24  18 bis 24 uhr aller 6 stunden syncen
 		 */
+	}
+
+	/**
+	 * init schedules on activation
+	 *
+	 * @wp-hook wp_doodle_polls_activate
+	 * @param WP_Doodle_Polls
+	 * @return void
+	 */
+	public static function init_schedule( $plugin ) {
+
+		# try to syncronize the first occurence to full hours and
+		# 00:00 / 12:00 local time for daily/twice_daily events
+
+		# next local time midnight
+		$utc_offset = get_option( 'gmt_offset' );
+		$timezone   = get_option( 'timezone_string' );
+		$php_default_tz = date_default_timezone_get();
+
+
 
 	}
 
